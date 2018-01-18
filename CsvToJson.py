cc3c25b48a15fb05eb95e43032e9e0ca040649d7
Ian McNaughton <IanMcNaugh@gmail.com>
2018-01-17 22:43:06 -0600

    moved data processing logic into its own file

---
+++ CsvToJson.py
@@ -0,0 +1,47 @@
+from Models import ProcessorConfigs
+
+class CsvToJson:
+	noParentIdString = 'NO_PARENT_ID'
+
+	def convert(self, configs, rows):
+		dataMap = self.mapRows(configs, rows)
+		topLevelElements = self.nestDataMap(configs, dataMap)
+		return topLevelElements
+
+	def mapRows(self, configs, rows):
+		dataMap = dict({})
+
+		for row in rows:
+			parentId = row[configs.parentIdFieldName]
+			if(parentId == ''):
+				parentId = self.noParentIdString
+
+			if parentId in dataMap:
+				childeren = dataMap[parentId]
+				childeren.insert(0, row)
+				dataMap[parentId] = childeren
+			else:
+				dataMap[parentId] = [row]
+		return dataMap
+
+	def nestDataMap(self, configs, dataMap):
+		topLevelElements = []
+
+		for parentElement  in dataMap[self.noParentIdString]:
+			element = self.recCall(dataMap, parentElement, configs.idFieldName, configs.childFiledName)
+			topLevelElements.insert(0, element)
+
+		return topLevelElements
+
+	def recCall(self, dataMap, currentElement, idFieldName, childFiledName):
+		pId = currentElement[idFieldName]
+		if pId in dataMap.keys():
+			listOfChildren = dataMap[pId]
+			children = []
+			for child in listOfChildren:
+				child = self.recCall(dataMap, child, idFieldName, childFiledName)
+				children.insert(0, child)
+			currentElement[childFiledName] = children
+		else:
+			return currentElement
+		return currentElement
--- DataMarshaller.py
+++ DataMarshaller.py
@@ -1,52 +1,18 @@
 from Models import ProcessorConfigs
 from CsvParser import CsvFileParser
+from CsvToJson import CsvToJson
 import json
 
 class DataMarshaller:
 
 	def __init__(self, configs):
 		fileRows = CsvFileParser().getJsonListOfRows(configs.pathToFile)
-		
-		dataMap = dict({})
+		nestedElements = CsvToJson().convert(configs, fileRows)
 
-		noParentIdString = 'NO_PARENT_ID'
+		jsonString = json.dumps(nestedElements)
+		self.writeJsonToFile(jsonString, configs.outputFilePath)
 
-		for row in fileRows:
-			parentId = row[configs.parentIdFieldName]
-			if(parentId == ''):
-				parentId = noParentIdString
-
-			if parentId in dataMap:
-				childeren = dataMap[parentId]
-				childeren.insert(0, row)
-				dataMap[parentId] = childeren
-			else:
-				dataMap[parentId] = [row]
-
-		kingBigWigs = []
-
-		for topLevelElement  in dataMap[noParentIdString]:
-			kingBigWig = self.recCall(dataMap, topLevelElement, configs.idFieldName, configs.childFiledName)
-			kingBigWigs.insert(0, kingBigWig)
-
-		j = json.dumps(kingBigWigs)
-
-		fp = open(configs.outputFilePath, 'w')
-		fp.write(j)
+	def writeJsonToFile(self, jsonString, outputFilePath):
+		fp = open(outputFilePath, 'w')
+		fp.write(jsonString)
 		fp.close()
-
-
-	def recCall(self, dataMap, currentElement, idFieldName, childFiledName):
-		pId = currentElement[idFieldName]
-		if pId in dataMap.keys():
-			listOfChildren = dataMap[pId]
-			children = []
-			for child in listOfChildren:
-				child = self.recCall(dataMap, child, idFieldName, childFiledName)
-				children.insert(0, child)
-			currentElement[childFiledName] = children
-		else:
-			return currentElement
-
-		return currentElement
-
