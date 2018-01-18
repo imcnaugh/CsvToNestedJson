2fda968aa8c0d0413b8a3b69fc36bc051cc01d2b
Ian McNaughton <IanMcNaugh@gmail.com>
2018-01-17 22:51:03 -0600

    cleaning up bad method

--- CsvParser.py
+++ CsvParser.py
@@ -4,11 +4,11 @@ class CsvFileParser:
 	def getJsonListOfRows(self, pathToFile):
 		fp = open(pathToFile, 'r')
 		headerLine = fp.readline().rstrip('\n')
-		headerFields = self.splitLine(headerLine)
+		headerFields = headerLine.split(',')
 		listOfRecords = []
 		line = fp.readline().rstrip('\n')
 		while line:
-			lineFields = self.splitLine(line)
+			lineFields = line.split(',')
 			jsonObj = self.createJsonObject(headerFields, lineFields)
 			listOfRecords.insert(0, jsonObj)
 			line = fp.readline().rstrip('\n')
@@ -21,6 +21,3 @@ class CsvFileParser:
 		for x in range(0, len(headerFields)):
 			data[headerFields[x].strip()] = rowFields[x].strip()
 		return data
-
-	def splitLine(self, line):
-		return line.split(',')
